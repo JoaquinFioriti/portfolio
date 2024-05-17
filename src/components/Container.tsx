ec667e125e97290f17d5efcc75ef5d1f1adf534f
Joaquin Fioriti <joaquinfioritifreelance@gmail.com>
2024-05-17 15:46:16 -0300

    fix: if we want to add an image from a different domain, we need to specify that domain in next.config.js dile

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,8 @@
 import React, {FC} from 'react'
 import Project from './Project'
+import img from '../../public/techiflo.png'
+import Image from 'next/image'
+
 
 interface ContainerProps {
     className?:string
@@ -10,9 +13,9 @@ const Container: FC<ContainerProps> = ({className}) => {
         <div className={'my-10 w-full ' + className}>
         <p className=' text-5xl text-center'>Projects</p>
         <div className='flex flex-col mt-5 items-center gap-10'>
-            <Project className='p-4 w-1/2 flex justify-center border' title='Te chiflo' />
-            <Project className='p-4 w-1/2 flex justify-center border' title='Backtesting Trading' />
-            <Project className='p-4 w-1/2 flex justify-center border' title='Software Company' />
+            <Project className='p-4 w-1/2 flex justify-center border rounded-lg' title='Te chiflo' image={<Image alt='Te chiflo' src='https://ethic.es/wp-content/uploads/2023/03/imagen.jpg' width="100" height="64"/>}/>
+            <Project className='p-4 w-1/2 flex justify-center border rounded-lg' title='Backtesting Trading' />
+            <Project className='p-4 w-1/2 flex justify-center border rounded-lg' title='Software Company' />
         </div>
         </div>
     )
